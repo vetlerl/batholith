15b3b79bdc02edd1482879043133c0253f248bfc
vetlerl <rambergl@etud.insa-toulouse.fr>
2023-09-05 22:32:11 +0200

    Added different folders mentioned in .gpr file. Also created ui package with first proc : a css style adder.

---
+++ src/user_interfaces.adb
@@ -0,0 +1,64 @@
+--GTKADA, Basic
+with Gtk.Widget;        use Gtk.Widget;
+
+--GTKADA, Style
+with Gtk.Css_Provider;  use Gtk.Css_Provider;
+with Gtk.Style_Context; use Gtk.Style_Context;
+
+--GTKADA, Glib
+with Glib;              use Glib;
+with Glib.Error;        use Glib.Error;
+
+--ADA I/O
+with Ada.Text_IO;       use Ada.Text_IO;
+
+
+package body User_Interfaces is
+
+   
+   -----------------------------------------------------------------------------
+   ----              _     _    _____          ----
+   ----     /\      | |   | |  / ____|         ----
+   ----    /  \   __| | __| | | |     ___ ___  ----
+   ----   / /\ \ / _` |/ _` | | |    / __/ __| ----
+   ----  / ____ \ (_| | (_| | | |____\__ \__ \ ----
+   ---- /_/    \_\__,_|\__,_|  \_____|___/___/ ----
+   ----                                        ----
+   -----------------------------------------------------------------------------
+   -- ASCII Text Art Generator - Text Kool - Big
+   -----------------------------------------------------------------------------
+   procedure Add_Css(W: access Gtk_Widget_Record'Class; Address: in String) is
+      Css :   Gtk_Css_Provider;
+      Dummy : Boolean;
+      Error : aliased GError := null;
+      
+   begin
+      --creation of Css-provider
+      Gtk_New(Css);
+      
+      --importation from string
+      Dummy := Load_From_Path(Css, Address, Error'Access); 
+      
+      --checking
+      if Dummy then
+         Put_Line("Success in loading css style from :");
+      else
+         Put_Line("Failure to load css style from :");
+      end if;
+      Put_Line(Address);
+      
+      --removing precedent style
+      null;
+      
+      --applying style
+      if Error = null then
+         Get_Style_Context(W).Add_Provider(+Css, Guint'Last); 
+      else
+         Put_Line("Error: " & Get_Message(Error));
+         Error_Free(Error);
+      end if;
+      
+   end Add_Css;
+   -----------------------------------------------------------------------------
+
+end User_Interfaces;
